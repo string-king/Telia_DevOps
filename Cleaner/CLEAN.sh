452b4e678e7ab025d2c021520d703506b5d0d7a0
laflame210 <king.robert4224@gmail.com>
2024-04-21 20:57:55 +0300

    Rearranged files and removed duplicate CLEAN.sh

--- Cleaner/CLEAN.sh
+++ Cleaner/CLEAN.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+echo "Initiating cleanup..."
+
 current_date=$(date +%Y%m%d)
 
 archive_filename="archive_$current_date.tar.gz"
 
 # Archive all files (excluding CLEAN.sh) that are older than the current date
-find . -maxdepth 1 -type f ! \( -iname "CLEAN.sh" -o -iname "README.md" \) -print0 | tar -czvf "$archive_filename" --null -T -
+find . -maxdepth 1 -type f ! -name "CLEAN.sh" ! -newermt $(date +%Y-%m-%d) -print0 | tar -czvf "$archive_filename" --null -T -
 
 # Move the archive to the /archive subdirectory
 mkdir -p archive
 mv "$archive_filename" archive/
 
 # Delete the files that were included in the archive
-find . -maxdepth 1 -type f ! \( -iname "CLEAN.sh" -o -iname "README.md" \) -delete
+find . -maxdepth 1 -type f ! -name "CLEAN.sh" ! -newermt $(date +%Y-%m-%d) -delete
+
+echo "Finished cleanup."
